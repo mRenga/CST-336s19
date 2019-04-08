1b40e2b1898551dbed10650c1922cdc3966436ca
Maximillian Renga <zizco27@gmail.com>
2019-04-08 19:07:42 +0000

    p8

---
+++ practice/p8/api/check_word.php
@@ -0,0 +1,8 @@
+<?php
+//Used to check the letter the user inputed in the form, and if the letter is in the word
+//Should return an array of booleans as the api
+include 'dbConnection.php';
+
+
+
+?>
